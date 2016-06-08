a77194da9a7d81716d7e13f1272ca2c9a0566477
Prashant Tiwari <prashaantt@gmail.com>
2016-06-08 17:25:19 +0530

    Adds TSX IntelliSense, fixes #4

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import { NpmIntellisense } from './NpmIntellisense';
 export function activate(context: ExtensionContext) {
 	const provider = new NpmIntellisense();
 	const triggers = ['"', '\''];
-    const selector = ['typescript', 'javascript', 'javascriptreact'];
+    const selector = ['typescript', 'javascript', 'javascriptreact', 'typescriptreact'];
 	context.subscriptions.push(languages.registerCompletionItemProvider(selector, provider, ...triggers));
 }
 
