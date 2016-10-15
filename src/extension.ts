db4a7d680d1567b4bd9092ef46933274f38e9b03
chris <chris@chriss-MacBook.local>
2016-10-15 08:33:22 +0200

    import workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,9 @@
 'use strict';
-import { ExtensionContext, languages, DocumentSelector } from 'vscode';
+import { ExtensionContext, languages, DocumentSelector, workspace } from 'vscode';
 import { NpmIntellisense } from './NpmIntellisense';
 
 export function activate(context: ExtensionContext) {
-	if (vscode.workspace.rootPath) {
+	if (workspace.rootPath) {
 		const provider = new NpmIntellisense();
 		const triggers = ['"', '\''];
 	    const selector = ['typescript', 'javascript', 'javascriptreact', 'typescriptreact'];
