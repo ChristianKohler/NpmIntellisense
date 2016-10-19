8e5e5445e8bc7b5518c8b5d3f2d04d4299ea88ed
Tomasz Kasperek <kasperekt@gmail.com>
2016-10-19 21:11:47 +0200

    Fix completion without semicolon
    Fixes #10

--- src/PackageCompletionItem.ts
+++ src/PackageCompletionItem.ts
@@ -12,7 +12,7 @@ export default class PackageCompletionItem extends CompletionItem {
 
   importStringRange(document: TextDocument, position: Position): Range {
     const line = getCurrentLine(document, position);
-    const textToPosition = line.substring(0, position.character - 1);
+    const textToPosition = line.substring(0, position.character);
     const quoatationPosition = Math.max(textToPosition.lastIndexOf('\"'), textToPosition.lastIndexOf('\''));
     
     const startPosition = new Position(position.line, quoatationPosition + 1);
