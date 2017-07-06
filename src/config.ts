f1942dbd523ef242bbb5a347c8145e03dc9324df
Katy Moe <katy@katy.moe>
2017-07-06 11:37:00 +0100

    correct spelling of built in libs flag

--- src/config.ts
+++ src/config.ts
@@ -4,24 +4,24 @@ export interface Config {
     scanDevDependencies?: boolean,
     recursivePackageJsonLookup?: boolean,
     packageSubfoldersIntellisense?: boolean,
-    showBuildInLibs?: boolean,
+    showBuiltInLibs?: boolean,
     importES6?: boolean,
     importQuotes?: string,
     importLinebreak?: string,
     importDeclarationType?: string
 }
 
-export function getConfig() : Config {
+export function getConfig(): Config {
     const configuration = workspace.getConfiguration('npm-intellisense');
 
     return {
         scanDevDependencies: configuration['scanDevDependencies'],
         recursivePackageJsonLookup: configuration['recursivePackageJsonLookup'],
         packageSubfoldersIntellisense: configuration['packageSubfoldersIntellisense'],
-        showBuildInLibs: configuration['showBuildInLibs'],
+        showBuiltInLibs: configuration['showBuiltInLibs'],
         importES6: configuration['importES6'],
-        importQuotes:configuration['importQuotes'],
-        importLinebreak:configuration['importLinebreak'],
+        importQuotes: configuration['importQuotes'],
+        importLinebreak: configuration['importLinebreak'],
         importDeclarationType: configuration['importDeclarationType']
     };
 }
