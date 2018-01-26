37871b2bf46b1a2d42bf0ebe034eb4deaa29f18b
Douglas Rogers <doug@morrisonagency.com>
2018-01-26 10:23:29 -0500

    turned importQuotes into options

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export interface Config {
     packageSubfoldersIntellisense?: boolean,
     showBuildInLibs?: boolean,
     importES6?: boolean,
-    importQuotes?: string,
+    importQuotes?: "single" | "double",
     importLinebreak?: string,
     importDeclarationType?: string
 }
