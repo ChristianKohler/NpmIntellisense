1a2173f377261067f1fc1fc3246dfed906b4d786
Chris Kohler <mail@christiankohler.net>
2021-11-29 14:09:55 -0800

    fix: tests on windows

--- test/suite/provide.test.ts
+++ test/suite/provide.test.ts
@@ -98,6 +98,7 @@ function createFsf(): FsFunctions {
 
 function readJsonMock(path): Promise<any> {
   switch (path) {
+    case "C:\\User\\dummy\\project\\src\\package.json":
     case "/User/dummy/project/src/package.json":
       return Promise.resolve({
         dependencies: {
@@ -122,6 +123,8 @@ function isFileMock(path) {
     [
       "/User/dummy/project/src/package.json",
       "/User/dummy/project/package.json",
+      "C:\\User\\dummy\\project\\src\\package.json",
+      "C:\\User\\dummy\\project\\package.json",
     ].indexOf(path) !== -1
   );
 }
